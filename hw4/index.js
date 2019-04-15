a6fc770b3aec60824c83581303044b28a4bd35da
robbycostales1@gmail.com <robbycostales1>
2019-04-15 17:46:45 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -12,7 +12,9 @@ const examples = {
   'example2':
       'Buda-Pesth seems a wonderful place.',
   'example3':
-      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
+      'Scepticism was as much the result of knowledge, as knowledge is of scepticism.', 
+  'example4':
+      'When it healed, and Jem's fears of never being able to play football were assuaged, he was seldom self-conscious about his injury.'
 };
 
 function status(statusText) {
@@ -47,11 +49,11 @@ function doPredict(predict) {
   const result = predict(textField.value);
   score_string = "Class scores: ";
   for (var x in result.score) {
-    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
+    score_string += x + " ->  " + result.score[x].toFixed(4) + ", "
   }
   //console.log(score_string);
   status(
-      score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
+      score_string + ' elapsed: ' + result.elapsed.toFixed(4) + ' ms)');
 }
 
 function prepUI(predict) {
